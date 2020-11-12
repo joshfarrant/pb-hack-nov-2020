da46789fe18d3b69dae7206f5ac1d7b392da35bd
Josh Farrant <josh@farrant.me>
2020-11-12 10:38:11 +0000

    Tidying up

--- src/components/templates/HomePage/home-page.tsx
+++ src/components/templates/HomePage/home-page.tsx
@@ -3,15 +3,11 @@ import { NextSeo } from 'next-seo';
 
 import { Layout } from '../../templates/Layout';
 
-import { TProps } from './home-page.types';
 // import { StyledList, StyledPost } from './home-page.styled';
 
-export const HomePage = ({ posts }: TProps): ReactElement => (
+export const HomePage = (): ReactElement => (
   <>
-    <NextSeo
-      title='PB Hack'
-      description='Site description'
-    />
+    <NextSeo title="PB Hack" description="Site description" />
     <Layout>
       <h1>Hello, world!</h1>
     </Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import { ReactElement } from 'react';
 
 import { HomePage } from '../components/templates/HomePage';
 
-type TProps = {};
-
 const Index = (props: TProps): ReactElement => <HomePage {...props} />;
 
 export default Index;
