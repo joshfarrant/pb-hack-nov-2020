38764fc60b57b6735e6e7ccbcdbfef411dc8dafd
Liam Myles <liamthemonkey92@gmail.com>
2020-11-12 15:10:01 +0000

    Move card styles

--- src/components/PropertyCard/property-card.styled.ts
+++ src/components/PropertyCard/property-card.styled.ts
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
 
-export const StyledTab = styled.li`
-  flex: 1;
-  padding: 1rem 0;
-  color: ${properties => properties.theme.color.grey6};
+export const CardDiv = styled.div`
+  border-radius: 25px;
 
-  &:focus {
-    outline: 0.1rem solid ${properties => properties.theme.color.purple};
-  }
+  box-shadow: 1px 6px 18px -8px #595959;
+  overflow: hidden;
+  max-width: 100%;
+`;
 
-  &[aria-selected='true'] {
-    color: ${properties => properties.theme.color.purple};
-    border-bottom: 0.3rem solid ${properties => properties.theme.color.purple};
-  }
+export const CardContentDiv = styled.div`
+  margin: 15px 10px 20px;
+`;
+export const Heading2 = styled.h2`
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0;
+`;
+export const Paragraph = styled.p`
+  font-size: 16px;
+  margin: 5px 0;
+`;
+
+export const Image = styled.img`
+  width: 100%;
 `;
--- src/components/PropertyCard/property-card.tsx
+++ src/components/PropertyCard/property-card.tsx
@@ -1,32 +1,12 @@
-import styled from 'styled-components';
-
 import { ReactElement } from 'react';
 import { TProperties } from './property-card.types';
-
-const CardDiv = styled.div`
-  border-radius: 25px;
-
-  box-shadow: 1px 6px 18px -8px #595959;
-  overflow: hidden;
-  max-width: 100%;
-`;
-
-const CardContentDiv = styled.div`
-  margin: 15px 10px 20px;
-`;
-const Heading2 = styled.h2`
-  font-size: 20px;
-  font-weight: bold;
-  margin: 0;
-`;
-const Paragraph = styled.p`
-  font-size: 16px;
-  margin: 5px 0;
-`;
-
-const Image = styled.img`
-  width: 100%;
-`;
+import {
+  CardDiv,
+  CardContentDiv,
+  Heading2,
+  Paragraph,
+  Image,
+} from './property-card.styled';
 
 export function PropertyCard({
   imgSrc,
