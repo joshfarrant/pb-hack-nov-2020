57ec930a1c2a82ffd1443681819037df9b32c4dd
Josh Farrant <josh@farrant.me>
2020-11-12 14:19:13 +0000

    Fixed button buttonness

--- src/components/atoms/HeartAnimationButton/heart-button.styled.ts
+++ src/components/atoms/HeartAnimationButton/heart-button.styled.ts
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
-export const StyledButton = styled.div`
+export const StyledButton = styled.button`
   width: 32px;
   height: 32px;
   cursor: pointer;
+  margin: 0;
+  padding: 0;
+  box-shadow: none;
+  background: none;
+  border: none;
 `;
